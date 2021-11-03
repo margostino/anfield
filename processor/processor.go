08dcaf438ddcfd44cf0f9091ae1f1ee7564a0db7
Martin D'Agostino <martin.dagostino@klarna.com>
2021-11-03 23:04:29 +0100

    one goroutine per event URL

--- processor/processor.go
+++ processor/processor.go
@@ -7,6 +7,7 @@ import (
 	"github.com/margostino/anfield/domain"
 	"github.com/margostino/anfield/scrapper"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -15,57 +16,66 @@ var msgs = make(chan domain.Commentary)
 
 // Process TODO: spawn one process per URL
 func Process(urls []string) {
-	config := context.Config()
+	wg := common.WaitGroup(len(urls))
 	for _, url := range urls {
-		metadata := scrapper.GetMetadata(url)
-		commentaryUrl := url + config.Commentary.Params
-		event := &domain.Event{
-			Metadata: metadata,
-			Data:     make([]*domain.Commentary, 0),
-		}
-		go produce(commentaryUrl)
-		go consume(event)
-		<-done
+		go scan(url, wg)
 	}
+	wg.Wait()
+}
 
+func scan(url string, waitGroup *sync.WaitGroup) {
+	config := context.Config()
+	metadata := scrapper.GetMetadata(url)
+	commentaryUrl := url + config.Commentary.Params
+	event := &domain.Event{
+		Metadata: metadata,
+		Data:     make([]*domain.Commentary, 0),
+	}
+	go produce(commentaryUrl)
+	go consume(event)
+	<-done
+	waitGroup.Done()
 }
 
 // TODO: implement proper stop in loop but scan all partial events
 func produce(url string) {
-	sent := make([]uint32, 0)
-	matchInProgress := true
+	countDown := 0
 	endOfEvent := false
+	matchInProgress := true
+	sent := 0
+	eventName := strings.Split(url, "/")[7]
 	stopFlag := context.Config().Realtime.StopFlag
 	graceEndTime := context.Config().Realtime.GraceEndTime
-	countDown := 0
-	eventName := strings.Split(url, "/")[7]
+
 	fmt.Printf("======== START: %s ========\n", eventName)
+
 	for ok := true; ok; ok = matchInProgress {
+		if endOfEvent && countDown == 0 {
+			time.Sleep(graceEndTime * time.Millisecond)
+			countDown += 1
+		} else if endOfEvent && countDown == context.Config().Realtime.CountDown {
+			matchInProgress = false
+			break
+		}
 		rawEvents := scrapper.GetEvents(url)
 		commentaries := normalize(*rawEvents)
-		for _, commentary := range commentaries {
-			newHash := common.Hash(commentary.Comment)
-			if !common.InSlice(sent, newHash) {
-				msgs <- *commentary
-				sent = append(sent, newHash)
-				if strings.Contains(commentary.Comment, stopFlag) {
-					endOfEvent = true
-				}
-			} else {
-				if endOfEvent && countDown == 0 {
-					time.Sleep(graceEndTime * time.Millisecond)
-					countDown += 1
-				} else if endOfEvent && countDown == context.Config().Realtime.CountDown {
-					matchInProgress = false
-					break
+		if sent != len(commentaries) {
+			for _, commentary := range commentaries {
+				newHash := common.Hash(commentary.Comment)
+				if !common.InSlice(sent, newHash) {
+					msgs <- *commentary
+					sent += 1
+					if strings.Contains(commentary.Comment, stopFlag) {
+						endOfEvent = true
+					}
 				}
 			}
 		}
 	}
+
 	fmt.Printf("======== END: %s ========\n", eventName)
-	fmt.Println("Before closing channel")
+
 	close(msgs)
-	fmt.Println("Before passing true to done")
 	done <- true
 }
 
@@ -74,7 +84,13 @@ func consume(event *domain.Event) {
 		msg := <-msgs
 		time.Sleep(100 * time.Millisecond)
 		event.Data = append(event.Data, &msg)
-		fmt.Println(msg.Comment)
+
+		if msg.Time == "" && msg.Comment != "" {
+			fmt.Printf("# %s\n", msg.Comment)
+		} else {
+			fmt.Printf("%s - %s\n", msg.Time, msg.Comment)
+		}
+
 		//source.WriteOnFileIfUpdate(event)
 	}
 }
--- realtime/realtime.go
+++ realtime/realtime.go
@@ -15,19 +15,6 @@ func main() {
 	source.Initialize()
 	browser := scrapper.Browser()
 	defer browser.MustClose()
-
-	//summaryPage := browser.MustPage("https://www.livescore.com/en/football/england/premier-league/wolverhampton-wanderers-vs-everton/450665/?tab=summary-commentary")
-	//summaryPage.MustElement(".Summary_toggleAllComments__1M7FV").MustElement("*").MustClick()
-	//rawEvents := summaryPage.MustElement(".Summary_blockWrapper__1P4fu").MustText()
-	////rawEvents := summaryPage.MustElement(".Summary_blockWrapper__1P4fu").MustText()
-	////rawEvents := summaryPage.MustElements("*")
-	//events := strings.Split("nil", "\n")
-	//println(rawEvents)
-	////for _, element := range rawEvents {
-	////	println(element.MustText())
-	////}
-	//println(events)
-
 	urls := make([]string, 0)
 	if config.Realtime.Matches != nil {
 		baseUrl := config.Source.Url
